92bc2114d365f4ade638cd328a558e1e61c438c8
Ktaewon <dydalehdxo@naver.com>
2022-09-21 17:50:09 +0900

    feat: jenre, comment models are added

---
+++ server/models/jenres.js
@@ -0,0 +1,49 @@
+'use strict';
+const { Model, fn } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Jenre extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // models.Melody.belongsTo(models.User, {
+      //   foreignKey: 'email',
+      // });
+      // models.Melody.hasMany(models.Submelody, {
+      //   foreignKey: 'melody_id',
+      // });
+    }
+  }
+  Jenre.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: fn('now'),
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Jenre',
+    }
+  );
+  return Jenre;
+};
