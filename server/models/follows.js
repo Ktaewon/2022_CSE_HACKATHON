dace59c3067a590c0110047e75ac158408a15959
PARKYUNJU <pyunj6767@gmail.com>
2022-09-21 17:51:51 +0900

    follow,likes,instrument

---
+++ server/models/follows.js
@@ -0,0 +1,23 @@
+'use strict';
+const { Follow, fn } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Follow extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+  }
+  Follow.init({
+    follower: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+
+    followee: {
+      allowNull: false,
+      type: DataTypes.StRING,
+    },
+  });
+  return Follow;
+};
