3c15322267ce3aed6499461f1fce2f12dad73dc1
Ktaewon <dydalehdxo@naver.com>
2022-09-21 16:40:30 +0900

    feat: user model is added

--- server/models/users.js
+++ server/models/users.js
@@ -1,9 +1,65 @@
 'use strict';
-
-import { Sequelize, Model, DataTypes } from 'sequelize';
-
-const sequelize = new Sequelize('sqlite::memory:');
-const User = sequelize.define('User', {
-  username: DataTypes.STRING,
-  birthday: DataTypes.DATE,
-});
+const { Model, fn } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // static associate(models) {
+    //   // define association here
+    //   models.User.hasMany(models.Item, {
+    //     foreignKey: 'user_email',
+    //   });
+    //   models.User.hasMany(models.LeaseContract, {
+    //     foreignKey: 'user_email',
+    //   });
+    // }
+  }
+  User.init(
+    {
+      email: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      salt: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      phone: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      is_valid: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: fn('now'),
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+  return User;
+};
